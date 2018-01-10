8551e33278c580db680fc294fd0174683d6fcd57
Dmytro Kardashuk <dkardashuk@cogniance.com>
2018-01-11 00:46:18 +0200

    cleanup

--- app/scripts/boot.js
+++
@@ -1,4 +0,0 @@
-var browser_1 = require('angular2/platform/browser');
-var my_app_ts_1 = require('./components/my-app.ts');
-browser_1.bootstrap(my_app_ts_1.App);
-//# sourceMappingURL=boot.js.map
--- app/scripts/boot.ts
+++
@@ -1,5 +0,0 @@
-import {bootstrap}      from 'angular2/platform/browser';
-import {App}   from './components/my-app.ts';
-import {uiFormField}   from './components/form-field.ts';
-
-bootstrap(App);
